25b1971823dee18949e1f3eddb09091caa9d474a
Vaishnavish05 <119428970+Vaishnavish05@users.noreply.github.com>
2023-09-04 19:40:34 +0530

    Add files via upload
    A Python-based To-Do List application that helps you stay organized and manage your tasks efficiently. This application allows you to add, edit, and remove task.
    - Create, edit, and delete tasks.
    - Mark tasks as completed to keep track of your progress.
    - Simple and intuitive command-line interface.
    - Store your to-do list data locally

---
+++ python to-do list.py
@@ -0,0 +1,39 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def add_task():
+    task = entry.get()
+    if task:
+        listbox.insert(tk.END, task)
+        entry.delete(0, tk.END)
+    else:
+        messagebox.showwarning("Warning", "Please enter a task.")
+
+
+def remove_task():
+    try:
+        selected_task_index = listbox.curselection()[0]
+        listbox.delete(selected_task_index)
+    except IndexError:
+        messagebox.showwarning("Warning", "Please select a task to remove.")
+
+
+root = tk.Tk()
+root.title("To-Do List")
+
+
+listbox = tk.Listbox(root, selectbackground="light blue", selectmode=tk.SINGLE, bg="white", fg="black", selectforeground="black", selectborderwidth=0)
+listbox.pack(pady=10, padx=10, fill=tk.BOTH, expand=True)
+
+
+entry = tk.Entry(root, width=40)
+entry.pack(pady=10, padx=10, fill=tk.BOTH)
+
+
+add_button = tk.Button(root, text="Add Task", command=add_task, bg="green", fg="white")
+remove_button = tk.Button(root, text="Remove Task", command=remove_task, bg="red", fg="white")
+
+add_button.pack(pady=5, padx=10, fill=tk.BOTH)
+remove_button.pack(pady=5, padx=10, fill=tk.BOTH)
+
+root.mainloop()
