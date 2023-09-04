94258fbb8d184a580ed22e7e70dd7eefb525fe33
Vaishnavish05 <119428970+Vaishnavish05@users.noreply.github.com>
2023-09-04 19:59:16 +0530

    Update python to-do list.py

--- python to-do list.py
+++ python to-do list.py
@@ -9,7 +9,6 @@ def add_task():
     else:
         messagebox.showwarning("Warning", "Please enter a task.")
 
-
 def remove_task():
     try:
         selected_task_index = listbox.curselection()[0]
@@ -17,12 +16,10 @@ def remove_task():
     except IndexError:
         messagebox.showwarning("Warning", "Please select a task to remove.")
 
-
 root = tk.Tk()
 root.title("To-Do List")
 
-
-listbox = tk.Listbox(root, selectbackground="light blue", selectmode=tk.SINGLE, bg="white", fg="black", selectforeground="black", selectborderwidth=0)
+listbox = tk.Listbox(root, selectbackground="light blue", selectmode=tk.SINGLE)
 listbox.pack(pady=10, padx=10, fill=tk.BOTH, expand=True)
 
 
@@ -30,8 +27,8 @@ entry = tk.Entry(root, width=40)
 entry.pack(pady=10, padx=10, fill=tk.BOTH)
 
 
-add_button = tk.Button(root, text="Add Task", command=add_task, bg="green", fg="white")
-remove_button = tk.Button(root, text="Remove Task", command=remove_task, bg="red", fg="white")
+add_button = tk.Button(root, text="Add Task", command=add_task)
+remove_button = tk.Button(root, text="Remove Task", command=remove_task)
 
 add_button.pack(pady=5, padx=10, fill=tk.BOTH)
 remove_button.pack(pady=5, padx=10, fill=tk.BOTH)
